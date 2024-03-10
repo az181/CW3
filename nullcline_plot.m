88e191630c4e0a70afcd5049ea2005e5ac639221
ng451 <146185240+ng451@users.noreply.github.com>
2024-03-10 15:10:35 +0000

    Add files via upload

--- nullcline_plot.m
+++ nullcline_plot.m
@@ -3,15 +3,15 @@ y = x + 1;
 a = -1.5:1.5;
 b = 4 - 8*a;
 hold on
-plot(x,y, 'r')
-plot(a,b, 'b')
+plot(x,y, 'b')
+plot(a,b, 'r')
 xline(0, 'b')
 yline(0,'r')
 grid on
 title('Plot of Nullclines')
 xlabel('x')
 ylabel('y')
-legend('xdot nullcline', 'ydot nullcline')
+legend('ydot nullcline', 'xdot nullcline')
 
 x = [-1 0];
 y = [0 0];
