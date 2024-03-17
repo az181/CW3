20f2555e651ab561d909c6ae2b32b62f9746f664
ng451 <146185240+ng451@users.noreply.github.com>
2024-03-17 17:00:02 +0000

    Add files via upload

---
+++ manifolds.m
@@ -0,0 +1,19 @@
+function ODE
+t0 = 0;
+tfinal = 10;
+xy0 = [0.00001;3.99999];
+
+[t, xy] = ode23(@my0DE, [t0 ; tfinal], xy0);
+plot(xy(:,1), xy(:,2))
+hold on
+plot(0, 4, '.', 'LineWidth', 10, 'MarkerSize', 20)
+grid on
+
+end
+
+
+function dt = my0DE(t, xy)
+dt = [1;3];
+dt(1) =  xy(1) .* (-1 - xy(1) + xy(2));
+dt(2) =  xy(2) .* (4 - xy(2) - 8 * xy(1));
+end
