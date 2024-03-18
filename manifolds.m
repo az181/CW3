be0b563733570cee970c838f0c2a7a6c9e422dd7
ng451 <146185240+ng451@users.noreply.github.com>
2024-03-18 15:49:52 +0000

    Add files via upload

--- manifolds.m
+++ manifolds.m
@@ -8,6 +8,12 @@ plot(xy(:,1), xy(:,2))
 hold on
 plot(0, 4, '.', 'LineWidth', 10, 'MarkerSize', 20)
 grid on
+title('Unstable Manifold')
+xlabel('Exe-halibuts Population')
+ylabel('Wye-bait Population')
+legend('Unstable Manifold', 'Saddle Point')
+
+
 
 end
 
@@ -17,3 +23,4 @@ dt = [1;3];
 dt(1) =  xy(1) .* (-1 - xy(1) + xy(2));
 dt(2) =  xy(2) .* (4 - xy(2) - 8 * xy(1));
 end
+
